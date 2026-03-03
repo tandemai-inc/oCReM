bfa9682ac912b00914b6a5bc5e59a4981ed2be77
Yinhui.Yi10086 <yinhui.yi@tandemai.com>
2026-03-03 17:10:58 +0800

    enable create postgres db

--- ta_gen/db/postgres_manager.py
+++ ta_gen/db/postgres_manager.py
@@ -4,6 +4,7 @@
 
 import configparser
 import copy
+import traceback
 
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
@@ -60,7 +61,7 @@ class PostGresManager(DBManager):
             cursor.execute("""
                 CREATE TABLE IF NOT EXISTS env (
                     id BIGINT PRIMARY KEY,
-                    name TEXT,
+                    name TEXT UNIQUE NOT NULL,
                     radis SMALLINT
                 )
             """)
@@ -69,8 +70,8 @@ class PostGresManager(DBManager):
             cursor.execute("""
                 CREATE TABLE IF NOT EXISTS fragment (
                     id BIGINT PRIMARY KEY,
-                    core_smi TEXT,
-                    core_num_atoms BIGINT,
+                    core_smi TEXT UNIQUE NOT NULL,
+                    core_num_atoms INTEGER,
                     dist2 BIGINT
                 )
             """)
@@ -80,9 +81,15 @@ class PostGresManager(DBManager):
                 CREATE TABLE IF NOT EXISTS env_fragment (
                     env_id BIGINT REFERENCES env(id),
                     fragment_id BIGINT REFERENCES fragment(id),
-                    frequency BIGINT
+                    frequency BIGINT,
+                    PRIMARY KEY (env_id, fragment_id)
                 )
             """)
+
+            cursor.execute(
+                "CREATE INDEX IF NOT EXISTS idx_env_fragment_env_id ON env_fragment(env_id)")
+            cursor.execute(
+                "CREATE INDEX IF NOT EXISTS idx_env_fragment_fragment_id ON env_fragment(fragment_id)")
 
             conn.commit()
             cursor.close()
@@ -95,5 +102,82 @@ class PostGresManager(DBManager):
         self.conn = psycopg2.connect(**self.conn_params)
         self.cursor = self.conn.cursor()
 
+    def insert_new_env(self, envs, radius):
+        placeholders = ','.join(['%s'] * len(envs))
+        self.cursor.execute(
+            f"SELECT name, id FROM env WHERE name IN ({placeholders})",
+            envs
+        )
+        env_map = {row[0]: row[1] for row in self.cursor.fetchall()}
+        missing = [name for name in envs if name not in env_map]
+
+        if missing:
+            self.cursor.executemany(
+                "INSERT INTO env (name, radis) VALUES (%s, %s)",
+                [(name, radius) for name in missing]
+            )
+            self.cursor.execute(
+                f"SELECT name, id FROM env WHERE name IN ({','.join(['%s'] * len(missing))})",
+                missing
+            )
+            for row in self.cursor.fetchall():
+                env_map[row[0]] = row[1]
+
+        return env_map
+
     def insert_new_fragment(self, fragments):
         core_smis = list(fragments.keys())
+        placeholders = ','.join(['%s'] * len(core_smis))
+        self.cursor.execute(
+            f"SELECT core_smi, id FROM fragment WHERE core_smi IN ({placeholders})",
+            core_smis
+        )
+        fragment_map = {row[0]: row[1] for row in self.cursor.fetchall()}
+        missing = [name for name in core_smis if name not in fragment_map]
+        if missing:
+            data = [(name, fragments[name][0], fragments[name][1]) for name in missing]
+            self.cursor.executemany(
+                "INSERT INTO fragment (core_smi, core_num_atoms, dist2) VALUES (%s, %s, %s)",
+                data
+            )
+            self.cursor.execute(
+                f"SELECT core_smi, id FROM fragment WHERE core_smi IN ({','.join(['%s'] * len(missing))})",
+                missing
+            )
+            for row in self.cursor.fetchall():
+                fragment_map[row[0]] = row[1]
+
+        return fragment_map
+
+    def insert_env_fragment(self, env_fragment_counter, fragment_ids, env_ids):
+        upsert_data = [
+            (env_ids[env], fragment_ids[core_smi], count)
+            for (env, core_smi), count in env_fragment_counter.items()
+        ]
+
+        upsert_sql = """
+            INSERT INTO env_fragment (env_id, fragment_id, frequency)
+            VALUES (%s, %s, %s)
+            ON CONFLICT (env_id, fragment_id) DO UPDATE SET
+            frequency = env_fragment.frequency + EXCLUDED.frequency
+        """
+        self.cursor.executemany(upsert_sql, upsert_data)
+
+
+    def insert(self, envs, fragments, env_fragment_counter, radius):
+        self.connect_db()
+        try:
+            with self.conn:
+                env_ids = self.insert_new_env(envs, radius)
+                fragment_ids = self.insert_new_fragment(fragments)
+                self.insert_env_fragment(env_fragment_counter, fragment_ids, env_ids)
+                self.conn.commit()
+        except Exception as e:
+            traceback.print_exc()
+        finally:
+            self.close()
+
+    def close(self):
+        """close cursor and connection"""
+        self.cursor.close()
+        self.conn.close()
