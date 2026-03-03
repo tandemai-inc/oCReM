63b2a3226c48385217e500834d8df5fc54ce284e
Yinhui.Yi10086 <yinhui.yi@tandemai.com>
2026-03-03 16:01:48 +0800

    enable create sqlite db

--- ta_gen/db/__init__.py
+++ ta_gen/db/__init__.py
@@ -2,12 +2,14 @@
 # -*- coding:utf-8 -*-
 
 
-from ta_gen.db.postgres_manager import PostGresManager
-from ta_gen.db.sqlite3_manager import SqliteManager
 
 
 def create_db_manager(db_type):
     if db_type == "sqlite":
+        from ta_gen.db.sqlite3_manager import SqliteManager
+
         return SqliteManager()
     elif db_type == "postgres":
+        from ta_gen.db.postgres_manager import PostGresManager
+
         return PostGresManager()
